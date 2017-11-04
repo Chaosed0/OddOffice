d6adb71b8cc4e06bef020942435493ef09982bcc
Andrew Hung <druphoria@utexas.edu>
2017-11-04 16:19:00 -0700

    added bullet explosion

--- Assets/OddOffice/Scripts/Player/Bullet.cs
+++ Assets/OddOffice/Scripts/Player/Bullet.cs
@@ -6,10 +6,13 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 10;
     Rigidbody rb;
+    ParticleSystem explosion;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        explosion = GetComponentInChildren<ParticleSystem>();
+
         Vector3 velocity = transform.rotation * Vector3.forward * speed;
         rb.AddForce(velocity, ForceMode.VelocityChange);
     }
@@ -20,6 +23,7 @@ public class Bullet : MonoBehaviour
         if (health != null)
         {
             health.DealDamage(10);
+            explosion.Play();
             Destroy(this.gameObject);
         }
         else
