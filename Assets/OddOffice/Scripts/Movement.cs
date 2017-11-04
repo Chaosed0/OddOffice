4ee586a877d9ddcc8bc942d894936b5cda90adff
chaosed0 <chaosed0@gmail.com>
2017-11-03 22:32:00 -0700

    Very basic movement

--- Assets/OddOffice/Scripts/Movement.cs
+++ Assets/OddOffice/Scripts/Movement.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
+    public float moveForce;
 
-	// Use this for initialization
-	void Start () {
-		
+    Vector2 movement;
+    float yaw;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate()
+    {
+        transform.rotation = Quaternion.AngleAxis(yaw, Vector3.up);
+        rb.AddForce(transform.forward * movement.y * moveForce +  transform.right * movement.x * moveForce);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void Move(Vector2 movement, float yaw)
+    {
+        this.movement = movement;
+        if (this.movement.magnitude > 1)
+        {
+            this.movement = this.movement.normalized;
+        }
+        this.yaw += yaw;
+    }
 }
---
+++ Assets/OddOffice/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Vector2 lookSensitivity = new Vector2(1,1);
+
+    public Camera playerCamera;
+    public Movement movement;
+
+    private float cameraRotation = 0;
+    
+    void Start()
+    {
+        movement = GetComponent<Movement>();
+    }
+
+	void Update ()
+    {
+        float forward = Input.GetAxis("Vertical");
+        float strafe = Input.GetAxis("Horizontal");
+        float lookX = Input.GetAxis("Mouse X");
+        float lookY = Input.GetAxis("Mouse Y");
+
+        cameraRotation = Mathf.Clamp(cameraRotation - lookY, -89, 89);
+        playerCamera.transform.localRotation = Quaternion.AngleAxis(cameraRotation, Vector3.right);
+
+        movement.Move(new Vector2(strafe, forward), lookX);
+	}
+}
