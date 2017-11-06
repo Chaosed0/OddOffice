695da6c436a78692b7694643644b64fbfde4a139
Andrew Hung <druphoria@utexas.edu>
2017-11-05 16:06:15 -0800

    title screen finished

---
+++ Assets/OddOffice/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour {
+
+    public float alphaPerSecond;
+
+    private Image fadeImage;
+
+    void Awake()
+    {
+        Cursor.visible = false;
+        fadeImage = GameObject.Find("TitlePanel").GetComponent<Image>();
+    }
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(FadeOut());
+        }
+	}
+
+    IEnumerator FadeOut()
+    {
+        float alpha = 0;
+
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime * alphaPerSecond;
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, alpha);
+            yield return null;
+        }
+        SceneManager.LoadScene(1);
+    }
+}
