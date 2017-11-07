774705fc3eb9a089c116c5107e602b81e8b1bbb6
Andrew W Hung <druphoria@utexas.edu>
2017-11-06 19:38:56 -0800

    title screen music, render laser in separate camera

---
+++ Assets/OddOffice/Scripts/CameraSync.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSync : MonoBehaviour {
+
+    public Camera mainCam;
+    public Camera laserCam;
+	
+	void Update ()
+    {
+        laserCam.transform.SetPositionAndRotation(mainCam.transform.position, mainCam.transform.rotation);
+	}
+}
